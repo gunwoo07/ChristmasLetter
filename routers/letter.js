d1dcad6ddde3918f735c1ab356c6e4c6c12c7b14
gunwoo07 <studentdeveloper07@gmail.com>
2021-12-11 21:35:47 +0900

    finish let's make favicon

--- routers/letter.js
+++ routers/letter.js
@@ -1,16 +1,60 @@
 // Import express framework
 var express = require("express");
 var router = express.Router();
+// Import nodemailer
+var nodemailer = require("nodemailer");
 // Import fs to read files
 var fs = require("fs");
+
+// Set data.json path
+var dataPath = "./data/data.json";
+var email = "";
+var pass = "";
+var transporter = "";
+
+
+// Read data.json file
+fs.readFile(dataPath, "utf8", (err, jsonFile) => {
+    // Error
+    if (err) {
+        return console.log(err);
+    }
+
+    // Parse json file
+    let jsonData = JSON.parse(jsonFile);
+    email = jsonData.email.email;
+    pass = jsonData.email.pass;
+
+    transporter = nodemailer.createTransport({
+        service: "gmail",
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        auth: {
+            user: email,
+            pass: pass
+        }
+    });
+});
+
+
+// Send Email
+var sendMail = async (stdntId, letter) => {
+    await transporter.sendMail({
+        from: "장승 편지",
+        to: `21s${stdntId}@jangseung.sen.ms.kr`,
+        subject: "당신에게 편지가 도착했습니다!",
+        text: letter,
+        html: `<b>${letter.replace(/\n/gi, '<br>')}</b>`
+    });
+}
 
 
 // Start router
 router.get("/", (req, res) => {
     // Read code cookie
     let code = req.cookies.code;
-    // Set data.json path
-    let dataPath = "./data/data.json";
+
 
     // code is empty
     if (code == undefined) {
@@ -52,8 +96,6 @@ router.post("/send", (req, res) => {
     let letter = req.body.letter;
     let code = req.cookies.code;
     let stdntId = req.body.stdntId;
-    // Set data.json path
-    let dataPath = "./data/data.json";
 
 
     // Code is empty
@@ -90,6 +132,7 @@ router.post("/send", (req, res) => {
                 newJsonFile = JSON.stringify(jsonData, null, 4);
                 fs.writeFile(dataPath, newJsonFile, 'utf8', () => {
                     console.log(`[!]${user.name}(${user.code}) sent letter to ${stdntId}`);
+                    sendMail(stdntId, letter);
                     res.redirect("/finish");
                 });
                 return;
